192469ef3345995e975441e6f4fa5882b225a1d0
Kurt Nelson <kurtisnelson@gmail.com>
2015-01-21 11:46:33 -0500

    Finish setting up devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,18 @@
 class User
   include MongoMapper::Document
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
-
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :confirmable
 
   key :email, String
+  key :encrypted_password, String
+  key :current_sign_in_at, Time
+  key :last_sign_in_at, Time
+  key :current_sign_in_ip, String
+  key :last_sign_in_ip, String
+  key :sign_in_count, Integer
+  key :remember_created_at, Time
+  key :confirmed_at, Time
+  key :confirmation_sent_at, Time
+  key :confirmation_token, String
+  key :unconfirmed_email, String
 end
