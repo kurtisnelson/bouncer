540ff2fbdea8b4a990cfba3a9d19e60f9d432349
Kurt Nelson <kurtisnelson@gmail.com>
2015-01-27 11:45:03 -0500

    Add honeybadger context

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
+  before_filter do
+    Honeybadger.context({
+      user_id: current_user.id,
+      user_email: current_user.email
+    }) if current_user
+  end
+
   def authenticate_admin!
     return false unless authenticate_user!
     current_user.super_admin?
