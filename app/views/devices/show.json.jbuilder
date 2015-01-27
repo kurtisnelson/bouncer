388dcf458509e50509a4c86440cdb7f1ed1b4cf6
Kurt Nelson <kurtisnelson@gmail.com>
2015-01-27 17:43:21 -0500

    Return token on device create
    [#87026906]

--- app/views/devices/show.json.jbuilder
+++ app/views/devices/show.json.jbuilder
@@ -2,4 +2,5 @@ json.device do
   json.name @device.name
   json.serial @device.serial
   json.user_id @device.user_id
+  json.token @device.token
 end
--- spec/requests/devices_spec.rb
+++ spec/requests/devices_spec.rb
@@ -3,18 +3,23 @@ require 'request_helper'
 describe DevicesController do
   context 'user token' do
     let!(:access_token) { FactoryGirl.create(:machine_access_token) }
+    serial = "1234567890"
 
     it 'allows a device to be created' do
-      serial = "1234567890"
-      post devices_path(format: :json), access_token: access_token.token, device: {serial: '1234567890'}
+      post devices_path(format: :json), access_token: access_token.token, device: {serial: serial}
       expect(response).to be_success
       expect(json['device']['serial']).to eq serial
       expect(json['device']['user_id']).to eq access_token.resource_owner_id.to_s
     end
 
+    it 'assigns a token to a new device' do
+      post devices_path(format: :json), access_token: access_token.token, device: {serial: serial}
+      expect(json['device']['token']).to_not be nil
+    end
+
     it 'does not allow a duplicate device to be created' do
-      post devices_path(format: :json), access_token: access_token.token, device: {serial: '1234567890'}
-      post devices_path(format: :json), access_token: access_token.token, device: {serial: '1234567890'}
+      post devices_path(format: :json), access_token: access_token.token, device: {serial: serial}
+      post devices_path(format: :json), access_token: access_token.token, device: {serial: serial}
       expect(response.status).to eq 400
     end
   end
