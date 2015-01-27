fb9fdee59c00c180bc49ad73b688454c041e554c
Kurt Nelson <kurtisnelson@gmail.com>
2015-01-27 17:40:08 -0500

    Devices have a token
    [#87026906]

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,7 +1,16 @@
 class Device
   include MongoMapper::Document
+  before_create :generate_token
+
   belongs_to :user
   key :name, String
   key :serial, String, unique: true, required: true
+  key :token, String
   timestamps!
+
+  private
+
+  def generate_token
+    self.token = SecureRandom.hex 128
+  end
 end
