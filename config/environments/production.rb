8176b1e1a644f40ca81833cf43bc2cc6c19b0b6f
Kurt Nelson <kurtisnelson@gmail.com>
2015-01-28 13:28:26 -0500

    Adjust prod URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'mbouncer.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://bouncer.monsieur.co' }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
