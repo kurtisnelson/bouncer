0011259411ded5587e05773b19e9688989a0cf2b
Kurt Nelson <kurtisnelson@gmail.com>
2015-02-18 16:55:31 -0500

    Fix rollbar

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,13 +1,10 @@
 require 'rollbar/rails'
 Rollbar.configure do |config|
+  config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
+
+  # Here we'll disable in 'test':
   if Rails.env.test?
     config.enabled = false
   end
-
-  # config.person_method = "my_current_user"
-  # config.person_id_method = "my_id"
-  # config.person_username_method = "my_username"
-  # config.person_email_method = "my_email"
-
-  config.use_sidekiq
+  config.use_sidekiq 'queue' => 'rollbar'
 end
