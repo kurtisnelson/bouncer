24c0f205626288619b224782a12ec110b00aa2d8
Kurt Nelson <kurtisnelson@gmail.com>
2015-02-19 10:15:32 -0500

    Include name & email in JSON

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,4 @@
 class UserSerializer < JsonApiSerializer
-  attributes :id, :image, :phone, :super_admin, :confirmed_at
+  attributes :id, :image, :phone, :email, :name
+  attributes :super_admin, :confirmed_at, :email
 end
