2cbc434b10838b6ef6998d96fddd839cbfdab3bb
Kurt Nelson <kurtisnelson@gmail.com>
2015-03-03 20:21:21 -0500

    Allow start without analytics

--- config/initializers/analytics.rb
+++ config/initializers/analytics.rb
@@ -1,4 +1,8 @@
+if ENV['SEGMENT_KEY']
 Analytics = Segment::Analytics.new({
     write_key: ENV['SEGMENT_KEY'],
     on_error: Proc.new { |status, msg| print msg }
 })
+else
+  logger.warn "Segment Analytics disabled"
+end
