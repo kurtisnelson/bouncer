95251cbcf427347ce9535a03540c8f9287f06c21
Kurt Nelson <kurtisnelson@gmail.com>
2015-03-05 16:37:54 -0500

    Kill off old sidekiq on deploy

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,4 +1,5 @@
 run "bundle exec rake papertrail:start" # Start remote_syslog daemon to push logs to papertrail
 node[:deploy].each do |application, deploy|
+  run "bundle exec rake sidekiq:stop"
   run "bundle exec rake sidekiq:start"
 end
