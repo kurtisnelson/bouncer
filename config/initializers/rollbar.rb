aa902d0f5011a256abfc5b70e02bb183150c59a8
Kurt Nelson <kurtisnelson@gmail.com>
2015-03-05 11:04:38 -0500

    Ignore 404s on rollbar

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,10 +1,11 @@
 require 'rollbar/rails'
 Rollbar.configure do |config|
   config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
-
+  config.exception_level_filters.merge!('ActionController::RoutingError' => 'ignore')
   # Here we'll disable in 'test':
   if Rails.env.test?
     config.enabled = false
   end
+
   config.use_sidekiq 'queue' => 'rollbar'
 end
