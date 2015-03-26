f494a08badc1c5be0956e14be95f860f8316b7ae
Kurt Nelson <kurtisnelson@gmail.com>
2015-03-26 11:46:38 -0500

    Centralize auth helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
-  include AuthorizationHelper
   protect_from_forgery with: :null_session
   skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
 
   UnauthenticatedError     = Class.new(ActionController::ActionControllerError)
   BadRequestError          = Class.new(ActionController::ActionControllerError)
+  UnauthorizedError        = Class.new(ActionController::ActionControllerError)
 
   rescue_from UnauthenticatedError, with: :unauthenticated
   rescue_from BadRequestError, with: :bad_request
@@ -46,6 +46,37 @@ class ApplicationController < ActionController::Base
 
   def ssl_configured?
     Rails.env.production?
+  end
+
+  def authenticate_device_scope_or_admin!
+    return if current_user && current_user.super_admin?
+    doorkeeper_authorize!
+    raise UnauthorizedError unless doorkeeper_token.scopes.exists? :device
+  end
+
+  def authenticate_admin_or_owner! device
+    return if current_user && current_user.super_admin?
+    return if current_user && current_user.id = device.user_id
+    return if current_device && current_device.id = device.id
+    raise UnauthorizedError
+  end
+
+  def current_user(favourite=nil)
+    if super()
+      super()
+    elsif doorkeeper_token && doorkeeper_token.accessible?
+      User.find_by(id: doorkeeper_token.resource_owner_id)
+    else
+      nil
+    end
+  end
+
+  def current_device
+    if doorkeeper_token && doorkeeper_token.accessible?
+      Device.find_by(id: doorkeeper_token.resource_owner_id)
+    else
+      nil
+    end
   end
 
   private
