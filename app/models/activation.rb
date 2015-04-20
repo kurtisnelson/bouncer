65ee0800992bd2b1ae93fe848cb50794ac583904
Kurt Nelson <kurtisnelson@gmail.com>
2015-04-20 12:01:57 -0400

    Tighter validations on activations

--- app/models/activation.rb
+++ app/models/activation.rb
@@ -2,7 +2,9 @@ class Activation < ActiveRecord::Base
   belongs_to :unit
   belongs_to :user
   has_many :tokens, foreign_key: :resource_owner_id, class: Doorkeeper::ActivationToken
-  validates :unit_id, :device_id, :user_id, presence: true
+  validates :unit, presence: true
+  validates_associated :unit
+  validates :device_id, :user_id, presence: true
   validates_uniqueness_of :unit_id
   validates_uniqueness_of :device_id
 
